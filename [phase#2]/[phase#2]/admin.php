2984f5533dbfcbf0ce0040276ec384ff6019b9d3
matheo m <166307348+braysake@users.noreply.github.com>
2025-03-16 16:36:29 +0100

    Add files via upload

---
+++ [phase#2]/[phase#2]/admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<title> CY sland </title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+	<link rel="shortcut icon" type="image/x-icon" href="image/logo cy sland.png"/>
+</head>
+
+<body>
+	<?php
+	include("header.php")
+	?>
+
+	<main>
+		<section>
+			<h2>Admin side</h2>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, architecto.
+			</p>
+		</section>
+	</main>
+
+	<?php
+	include("footer.php")
+	?>
+</body>
+
+</html>
+<script src="script.js"></script>
---
+++ [phase#2]/[phase#2]/croisière3.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr"> <!--commentaires -->
+
+<head>
+	<title> CY sland </title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+	<link rel="shortcut icon" type="image/x-icon" href="image/logo cy sland.png"/>
+</head>
+
+<body>
+	<?php
+	include("header.php")
+	?>
+
+	<main>
+		<article>
+			<img class="description" src="image/carte_postal.png"/>
+			<p class="description">Ce voyage vous emmenera sur les plages de l'indonésie et ses centaines d'île, vous feront découvrir une flore et faune que vous n'aurait jamais vu.</p>
+			<label for="option 1 indonésie">Option de séjour 1</label>
+				<select name="qualité" id="qualité">
+					<option value="option 1">option 1</option>
+					<option value="option 2">option 2</option>
+					<option value="option 3">option 3</option>
+				</select>
+			<br>
+			<label for="option 2 indonésie">Option de séjour 2</label>
+				<select name="qualité" id="qualité">
+					<option value="option 1">option 1</option>
+					<option value="option 2">option 2</option>
+					<option value="option 3">option 3</option>
+				</select>
+			<br>
+			<label for="option 3 indonésie">Option de séjour 3</label>
+				<select name="qualité" id="qualité">
+					<option value="option 1">option 1</option>
+					<option value="option 2">option 2</option>
+					<option value="option 3">option 3</option>
+				</select>
+			<div class="clear"></div>
+			<p>
+				Voyage en indonésie
+			</p>
+		</article>
+	</main>
+
+	<?php
+	include("footer.php")
+	?>
+</body>
+
+</html>
+<script src="script.js"></script>
