b5c49f8ff18daa84ecb94c0869cd235d58b1a6fc
PseudoNori <166307840+PseudoNori@users.noreply.github.com>
2025-03-20 13:44:00 +0100

    Add files via upload

--- [phase#2]/detail_voyage.php
+++ [phase#2]/detail_voyage.php
@@ -1,28 +1,35 @@
 <?php
     include("header.php");
-    $id=$_GET[voyage];
-    $file=fopen("excel.csv",r);
+    $id=$_GET["voyage"];
+    $file=fopen("excel.csv",'r');
     for ($i=0; $i<=$id;$i++){
         $detail=fgets($file);
     }
     $tabvoyage=explode(";",$detail);
     $prix=intval($tabvoyage[3]);
-    if (isset($_GET[qualité])){
-        $qualité=$_GET[qualité];
+    if (isset($_GET["qualité"])){
+        $qualité=$_GET["qualité"];
         switch ($qualité) {
-            case économique:
+            case "économique":
                 $prix=intval($tabvoyage[3])-intval($tabvoyage[3])/2;
             break;
-            case moyen:
+            case "moyen":
                 $prix=intval($tabvoyage[3]);
             break;
-            case deluxe:
+            case "deluxe":
                 $prix=intval($tabvoyage[3])+intval($tabvoyage[3])/2;
             break;
             }
     }
-    echo "{$_get[voyage]}"."{$id} ";
-    echo "{$prix}";
+    if (isset($_GET["activité"])){
+        
+        $activité=$_GET["activité"];
+        foreach($activité as $act){
+            $prix+=100;
+        }
+    }
+    echo $id ;
+    echo " {$prix}";
     echo "<div class='container'>
                 <article>
                     <img class='rectangle' src='image/carte_postal.png'/>
@@ -35,9 +42,14 @@
                         ".$tabvoyage[5]."<br>
                         ".$tabvoyage[6]."<br>
                         ".$tabvoyage[7]."<br>
+                        à quelles activités souhaitez vous participer: <br>
+                        
                     </p>
                     <p class='description'>
                         <form method='get' action='detail_voyage.php'>
+                            <input type='checkbox' name='activité[]' value='".$tabvoyage[5]."' checked> ".$tabvoyage[5]."<br>
+                            <input type='checkbox' name='activité[]' value='".$tabvoyage[6]."' checked> ".$tabvoyage[6]."<br>
+                            <input type='checkbox' name='activité[]' value='".$tabvoyage[7]."' checked> ".$tabvoyage[7]."<br>
                             <label for='qualité du voyage'>qualité du voyage</label>
 							<select name='qualité' id='qualité'>
 								<option value='économique'>économique</option>
@@ -50,10 +62,10 @@
                                 <input type='submit' id='bouton_voyage' value='appliquer les filtres' />
                         </form>
                         Prix: <br>
-                        <h2>
+                        <h1>
                             "."{$prix}"."
 
-                        </h2>
+                        </h1>
                     <div class='clear'></div>
                     <p>
                         Voyage en thailand
