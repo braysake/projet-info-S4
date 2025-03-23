1f4203ac9b58af574961480da6c43167dd8c1fde
PseudoNori <166307840+PseudoNori@users.noreply.github.com>
2025-03-23 17:51:49 +0100

    Add files via upload

--- [phase#2]/resume_paiement.php
+++ [phase#2]/resume_paiement.php
@@ -1,68 +1,58 @@
 <?php
-include("variable.php");
+	include("header.php")
 ?>
-
 <!DOCTYPE html>
-<html lang="fr">
-<?php
-    include("header.php")
-?>
-
-<body>
-    <main>
-        <article>
-            <?php
-            $qualité=$_GET['qualité'];
-            $id=$_GET["voyage"];
-            $montant=$_GET['montant'];
-            $file=fopen('data/excel.csv','r');
-            for ($i=0; $i<=$id;$i++){
-                $detail=fgets($file);
-            }
-            $tabvoyage=explode(";",$detail);
-            echo "<p>
-                Vous avez reserver le voyage suivant: <br>
-                </p>
-                <h4>".$tabvoyage[0]."</h4> <br>
-                <p>
-                    avec les activités suivantes: <br>";
-            for($i=0;$i<$_GET['nb_act'];$i++){
-                echo "".$_GET[$i]."<br>";
-                    }
-
-            echo"avec une qualité de logement ".$qualité."<br>
-                au prix de: ".$montant."€ <br>
-                </p>
-                ";
-            $array_voyage=array($id+1,$qualité,$montant,$_GET['nb_act']);
-            for($i=0;$i<$_GET['nb_act'];$i++){
-                array_push($array_voyage,$_GET[$i]);
-            }
-            $out=fopen('data/'.$tab_inscrit[$_SESSION["id"]][0].'.csv','a+');
-            $tab_res=file('data/'.$tab_inscrit[$_SESSION["id"]][0].'.csv');
-            $check=0;
-            $str_data=implode(' ',$array_voyage);
-            for($i=0;$i<count($tab_res);$i++){
-                if("".$str_data."\n"==$tab_res[$i]){
-                    $check=1;
-                }
-            }
-            if($check!=1){
-                fwrite($out,implode(' ',$array_voyage));
-                fwrite($out,"\n");
-            }
-
-            ?>
-            <p>
-                La transaction à été effectuer, merci d'avoir utilisé CY-sland, nous vous souhaitons un agréable séjour.
+    <article>
+        <?php
+        if($_GET['status']!="accepted"){
+            header("Location: detail_voyage.php?voyage=".$_GET["voyage"]."&error=1");
+        }
+        $qualité=$_GET['qualité'];
+        $id=$_GET["voyage"];
+        $montant=$_GET['montant'];
+        $file=fopen('data/excel.csv','r');
+        for ($i=0; $i<=$id;$i++){
+            $detail=fgets($file);
+        }
+        $tabvoyage=explode(";",$detail);
+        echo "<p>
+            Vous avez reserver le voyage suivant: <br>
             </p>
-        </article>
-    </main>
+            <h4>".$tabvoyage[0]."</h4> <br>
+            <p>
+                avec les activités suivantes: <br>";
+        for($i=0;$i<$_GET['nb_act'];$i++){
+            echo "".$_GET[$i]."<br>";
+                }
 
-    <?php
-        include("footer.php")
-    ?>
-</body>
+        echo"avec une qualité de logement ".$qualité."<br>
+            au prix de: ".$montant."€ <br>
+            </p>
+            ";
+        $array_voyage=array($id+1,$qualité,$montant,$_GET['nb_act']);
+        for($i=0;$i<$_GET['nb_act'];$i++){
+            array_push($array_voyage,$_GET[$i]);
+        }
+        $out=fopen('data/profil_voyage.csv','a+');   
+        $tab_res=file('data/profil_voyage.csv');
+        $check=0;
+        $str_data=implode(' ',$array_voyage);
+        for($i=0;$i<count($tab_res);$i++){
+            if("".$str_data."\n"==$tab_res[$i]){
+                $check=1;
+            }
+        }
+        if($check!=1){
+            fwrite($out,implode(' ',$array_voyage));
+            fwrite($out,"\n");
+        }
 
-</html>
-<script src="script.js"></script>
+        ?>
+        <p>
+            La transaction à été effectuer, merci d'avoir utilisé CY-sland, nous vous souhaitons un agréable séjour.
+        </p>
+    </article>
+
+<?php
+	include("footer.php")
+?>
