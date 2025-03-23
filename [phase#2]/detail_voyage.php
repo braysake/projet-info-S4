be69f561a9088e6ccfe0c2352bbf6d44c08a1ac3
PseudoNori <166307840+PseudoNori@users.noreply.github.com>
2025-03-23 16:06:21 +0100

    Update detail_voyage.php

--- [phase#2]/detail_voyage.php
+++ [phase#2]/detail_voyage.php
@@ -55,8 +55,6 @@
     $http_activité=http_build_query($activité);
     $retour="http://localhost/resume_paiement.php?voyage=".$id."&qualité=".$qualité."&".$http_activité."&nb_act=".$nb_act."&session=s";
     $md5=md5($api."#".$transaction."#".$prix."#".$vendeur."#".$retour."#");
-    echo "<br>".$md5."<br>";
-    echo $prix;
     if(isset($_POST['verif'])){
         $_SESSION["paiement"]=1;
     }
